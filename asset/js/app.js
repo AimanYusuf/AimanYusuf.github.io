daba6575d2cee0375266b8540c6c7379b95e7b40
Aiman yusuf <aimanyusuf110@gmail.com>
2023-02-07 17:53:18 +0700

    Revisi 2

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,17 @@
+// hero gonta ganti
+const imgaes = [
+  "asset/img/hero.png",
+  "asset/img/hero2.png",
+  "asset/img/hero3.png",
+  "asset/img/hero4.png",
+  "asset/img/hero6.png",
+];
+const heroImages = document.querySelector(".img-hero");
+let imgIndex = 0;
+setInterval(() => {
+  imgIndex = (imgIndex + 1) % imgaes.length;
+  heroImages.src = imgaes[imgIndex];
+}, 5000);
 // Toogle class active
 const navMenu = document.querySelector(".nav-menu");
 
@@ -22,4 +36,3 @@ window.addEventListener("scroll", () => {
 function imgSlider(a) {
   document.querySelector(".t-shirt").src = a;
 }
-// Ketersediaan Fungsi
